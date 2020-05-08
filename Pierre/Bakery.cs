d8106c65db67172b9fc25ce74a1244a2123c6644
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-08 09:41:47 -0700

    namespace & classes framework

--- Pierre/Bakery.cs
+++ Pierre/Bakery.cs
@@ -5,10 +5,20 @@ namespace Bakery.Pierre
 {
   public class Bread
   {
+    public int Loaf { get; set; }
 
+    public Bread()
+    {
+      Loaf = loaf;
+    }
   }
   public class Pastry
   {
-    
+    public int Danish { get; set; }
+
+    public Pastry()
+    {
+      Danish = danish;
+    }
   }
 }
