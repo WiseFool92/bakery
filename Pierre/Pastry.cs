b91343db840004618c7869996357733db42e6c76
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-05-08 13:38:58 -0700

    remove old commented out logic

--- Pierre/Pastry.cs
+++ Pierre/Pastry.cs
@@ -42,29 +42,5 @@ namespace Bakery.Pierre
         }
       }
     }
-
-    // First Attempt
-
-    // public int NumberOfDanish { get; set; }
-    // public int CostPerDanish { get; set; }
-    // public int TotalDanish { get; set; }
-
-    // public Pastry()
-    // {
-    //   NumberOfDanish;
-    //   CostPerDanish = 2;
-    //   TotalDanish;
-    // }
-
-    // public int CalculateDanish()
-    // {
-    //   TotalDanish = 0;
-    //   TotalDanish = (NumberOfDanish * CostPerDanish);
-    //   if (NumberOfDanish == 3)
-    //   {
-    //     TotalDanish --;
-    //   }
-    //   return TotalDanish;
-    // }
   }
 }
